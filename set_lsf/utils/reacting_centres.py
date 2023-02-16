c72e33b52e81d02a9064eb6f8005ce2990ad51a2
ruslankotl <rk582@cam.ac.uk>
2023-02-16 12:28:55 +0000

    rename reacting centre  module

--- set_lsf/utils/reacting_centres.py
+++ set_lsf/utils/reacting_centres.py
@@ -19,20 +19,15 @@ def init_args():
 def parent_centres(reactant_smiles:str, product_smiles:str)->np.array(list):
     reactant = Chem.MolFromSmiles(reactant_smiles)
     product = Chem.MolFromSmiles(product_smiles)
-    centres = list()
-
     match = product.GetSubstructMatch(reactant)
-
-    # get the reactive atoms indices on product: in substructure, but with neighbours outside MCS
     
     reaction_sites = [atom.GetIdx() for atom in product.GetAtoms() \
          if (atom.GetIdx() in match) and \
          not all((n.GetIdx() in match) for n in atom.GetNeighbors())]
 
     # noting that reactant atoms have a through numbering 
-    for reactant_index, prod_index in enumerate(match):
-          if prod_index in reaction_sites:
-               centres.append(reactant_index)
+    centres = [reactant_index for reactant_index, prod_index in enumerate(match)
+    if prod_index in reaction_sites]
 
     return np.array(centres)
 
